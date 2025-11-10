c8b007c2699969ab28acbc25fdf33e218d5fa4c0
Yegor-77 <egorhmeluk77@gmail.com>
2025-11-10 18:23:21 +0100

    рпа

--- index-Y.easy.js
+++ index-Y.easy.js
@@ -25,4 +25,31 @@ button.addEventListener("click", () => {
 
 //2 harddd
 
-// не начав ще я це робити
+const field = document.querySelector(".fotball-div");
+const ball = field.querySelector("img");
+
+field.style.position = "relative";
+ball.style.position = "absolute";
+ball.style.transition = "0.1s linear";
+
+field.addEventListener("mousemove", (event) => {
+  const fieldRect = field.getBoundingClientRect();
+
+  const x = event.clientX - fieldRect.left;
+  const y = event.clientY - fieldRect.top;
+
+  const ballWidth = ball.offsetWidth;
+  const ballHeight = ball.offsetHeight;
+
+  let newX = x - ballWidth / 2;
+  let newY = y - ballHeight / 2;
+
+  if (newX < 0) newX = 0;
+  if (newY < 0) newY = 0;
+  if (newX > fieldRect.width - ballWidth) newX = fieldRect.width - ballWidth;
+  if (newY > fieldRect.height - ballHeight)
+    newY = fieldRect.height - ballHeight;
+
+  ball.style.left = `${newX}px`;
+  ball.style.top = `${newY}px`;
+});
